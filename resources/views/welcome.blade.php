f0d80dac91a6187087a993a0f334b01379a7c02a
Max <lazy_boyblue@hotmail.com>
2017-08-07 23:01:06 -0400

    Setup sass file collection for styles. Began working on home page template.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,42 +7,54 @@
 
         <title>Laravel</title>
 
-        <!--- CSS --->
-        <!-- Fonts -->
+        <!-- FONTS -->
+        <!-- Raleway -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!--- CSS --->
         <!-- Bootstrap 3.3.7 -->
         <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.css') }}" />
 
+        <!-- App -->
+        <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
+
         <!--- JAVASCRIPT --->
-        <!-- Bootstrap 3.3.7-->
-        <script type="text/javascript" src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+        <!-- Jquery 3.2.1 -->
+        <script type="text/javascript" src="{{ URL::asset('js/vendors/jquery-3.2.1.min.js') }}"></script>
+        <!-- Bootstrap 3.3.7 -->
+        <script type="text/javascript" src="{{ URL::asset('js/vendors/bootstrap.min.js') }}"></script>
+        <!-- Custom -->
+        <script type="text/javascript" src="{{ URL::asset('js/custom.js') }}"></script>
     </head>
+
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+        <header>
+            <div class="container">
+                <div class="row-fluid">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
-        </div>
+        </header>
+
+        <section>
+            <div class="banner-container">
+                <div class="overlay"></div>
+                <div class="banner-image"></div>
+            </div>
+
+            <div class="container">
+                <div class="row-fluid">
+
+                </div>
+            </div>
+        </section>
+
+        <footer>
+            <div class="container">
+                <div class="row-fluid">
+
+                </div>
+            </div>
+        </footer>
     </body>
 </html>
