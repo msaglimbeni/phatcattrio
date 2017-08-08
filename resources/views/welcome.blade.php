2e9772e2393e92e46daad58d8b20af97365492e1
Max <lazy_boyblue@hotmail.com>
2017-08-07 22:15:17 -0400

    Added bootstrap js/css/fonts to project.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,62 +7,15 @@
 
         <title>Laravel</title>
 
+        <!--- CSS --->
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <!-- Bootstrap 3.3.7 -->
+        <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.css') }}" />
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <!--- JAVASCRIPT --->
+        <!-- Bootstrap 3.3.7-->
+        <script type="text/javascript" src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
